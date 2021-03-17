debb57317d4943f8f7aadf7aaa0d80f09269bf12
kazuki-h <kazuki-h@weseek.co.jp>
2021-03-18 00:08:58 +0900

    form

--- components/form.jsx
+++ components/form.jsx
@@ -25,7 +25,38 @@ const ContactForm = () => {
                 }, 400);
             }}
           >
+              {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
 
+              }) => (
+                <form onSubmit={handleSubmit}>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                {errors.email && touched.email && errors.email}
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                {errors.password && touched.password && errors.password}
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </form>     
+              )}
           </Formik>
         </div>
     )

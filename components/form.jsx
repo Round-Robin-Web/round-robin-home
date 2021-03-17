cf32fb8cca3e5843e57eef1dc93374a902d1f4a5
kazuki-h <kazuki-h@weseek.co.jp>
2021-03-17 23:57:06 +0900

    first form

--- components/form.jsx
+++ components/form.jsx
@@ -4,7 +4,23 @@ import { Formik } from 'formik';
 const ContactForm = () => {
     return (
         <div>
+          <h1>Anywhere in your app!</h1>
+          <Formik
+            initialValues={{ email: '', password: ''}}
+            validate={values=> {
+                const errors = {};
+                if (!values.email) {
+                    errors.email = 'Required';
+                } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                    errors.email = 'Invalid email address';
+                }
+                return errors;
+            }}
+          >
 
+          </Formik>
         </div>
     )
 }

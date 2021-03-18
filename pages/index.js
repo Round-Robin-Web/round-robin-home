ea0f1b5a6733be4dbaa79d73c92c594f84441a3d
kazuki-h <kazuki-h@weseek.co.jp>
2021-03-18 13:45:07 +0900

    form excute

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.scss'
+import ContactForm from '../components/Form'
 
 export default function Home() {
   return (
@@ -53,6 +54,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <ContactForm />
     </Layout>
   )
 }

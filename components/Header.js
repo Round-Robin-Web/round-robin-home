c9d990a6b865e954437717a7a161e15e6b57debe
井上翼 <tsubasa.inoue.3b@stu.hosei.ac.jp>
2021-03-22 13:47:42 +0900

    a little adjustment in margin and padding

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,11 @@
 import Link from 'next/link'
-import Styles from './Header.module.scss'
-import utilStyles from '../Styles/utils.module.scss'
 import Image from 'next/image'
 
 export default function Header(){
     return(
-            <header className={Styles.header}>
-                <div className= 'd-inline-flex p-2 bd-highlight'>
-                    <div className="">
+            <header>
+                <div className= 'd-inline-flex p-2 ms-4 bd-highlight'>
+                    <div>
                         <Image
                             priority
                             src = '/images/RoundLogo.png'
@@ -15,11 +13,11 @@ export default function Header(){
                             height={110}
                         />
                     </div>
-                    <div className='pt-4 fs-2 fw-bold'>
+                    <div className='pt-4 mt-1 fs-2 fw-bold'>
                         <p>Round Robin</p>
                     </div>
                 </div>
-                <div className="d-flex flex-row bd-highlight mb-3 float-end fs-5 fw-bold mt-3 pt-1">
+                <div className="d-flex flex-row bd-highlight mb-3 float-end fs-5 fw-bold mt-3 pt-2 me-4">
                     <Link href='/'>
                         <a className="p-4 bd-highlight text-dark">Home</a>
                     </Link>
@@ -36,7 +34,6 @@ export default function Header(){
                         <a className="p-4 bd-highlight text-dark">Contact</a>
                     </Link>
                 </div>
-                <div classNmae={Styles.clear}></div>
             </header>
     )
 }

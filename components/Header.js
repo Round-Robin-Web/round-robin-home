6668afb3983d2f41c5c40528705f356f843b0f58
井上翼 <tsubasa.inoue.3b@stu.hosei.ac.jp>
2021-03-22 15:55:09 +0900

    a bit changes in styles

--- components/Header.js
+++ components/Header.js
@@ -4,34 +4,32 @@ import Image from 'next/image'
 export default function Header(){
     return(
             <header>
-                <div className= 'd-inline-flex p-2 ms-4 bd-highlight'>
-                    <div>
-                        <Image
-                            priority
-                            src = '/images/RoundLogo.png'
-                            width={110}
-                            height={110}
-                        />
-                    </div>
-                    <div className='pt-4 mt-1 fs-2 fw-bold'>
-                        <p>Round Robin</p>
-                    </div>
+                <div className= 'd-inline-flex p-2 ms-4'>
+                    <Image
+                        priority
+                        src = '/images/RoundLogo.png'
+                        width={110}
+                        height={110}
+                    />
+                    <h3 className='pt-4 mt-2 fs-2 fw-bold'>
+                        Round Robin
+                    </h3>
                 </div>
-                <div className="d-flex flex-row bd-highlight mb-3 float-end fs-5 fw-bold mt-3 pt-2 me-4">
+                <div className="d-flex flex-row mb-3 float-end fw-bold mt-4 pt-2 me-4">
                     <Link href='/'>
-                        <a className="p-4 bd-highlight text-dark">Home</a>
+                        <a className="p-3">Home</a>
                     </Link>
-                    <Link href='/posts/about'>
-                        <a className="p-4 bd-highlight text-dark">About</a>
+                    <Link href='/about'>
+                        <a className="p-3">About</a>
                     </Link>
-                    <Link href='/posts/member'>
-                        <a className="p-4 bd-highlight text-dark">Member</a>
+                    <Link href='/member'>
+                        <a className="p-3">Member</a>
                     </Link>
-                    <Link href='/posts/works'>
-                        <a className="p-4 bd-highlight text-dark">Works</a>
+                    <Link href='/works'>
+                        <a className="p-3">Works</a>
                     </Link>
-                    <Link href='/posts/contects'>
-                        <a className="p-4 bd-highlight text-dark">Contact</a>
+                    <Link href='/contects'>
+                        <a className="p-3">Contact</a>
                     </Link>
                 </div>
             </header>

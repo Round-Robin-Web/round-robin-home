012f8ab23514d3379795bf0b349f170c9543d68a
井上翼 <tsubasa.inoue.3b@stu.hosei.ac.jp>
2021-03-23 15:02:57 +0900

    create main page in front

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 import Image from 'next/image'
+import Link from 'next/link'
+import Styles from '../styles/index.module.scss'
 
 
 export async function getStaticProps(){
@@ -20,20 +22,25 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <div>
-        <Image
-          priority
-          src = '/images/homePage.png'
-          width={990}
-          height={1155}
-        />
-        <div >
-          <p >Live Freely</p>
-          <p >Grow Up With Friends!!</p>
-          <p >-駆け出しエンジニアの共同コミュニティ-</p>
+        <div className="float-end me-5 position-fixed top-0 end-0 mt-4 position-relative">
+          <Image
+            priority
+            src = '/images/homePage.png'
+            className="float-end"
+            width={950}
+            height={800}
+          />
         </div>
-        <div>
-          <button><p>メンバーになる</p></button>
-          <button><p>詳しく見る</p></button>
+        <div id={Styles.mainPassage} className="mt-5">
+          <p className={Styles.fontAdjust}>Live Freely</p>
+          <p className={Styles.fontAdjust}>Grow Up With Friends!!</p>
+          <p id={Styles.subPassage} className="fs-6 fw-bolder">-駆け出しエンジニアの共同コミュニティ-</p>
+          <Link href="#">
+            <a className={Styles.greenButton}>メンバーになる</a>
+          </Link>
+          <Link href="#">
+            <a className={Styles.blueButton}>詳しくみる</a>
+          </Link> 
         </div>
         
       </div>

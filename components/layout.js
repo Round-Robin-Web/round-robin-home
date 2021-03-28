bc8dd75e0fb0d4362480ac25368733e3fdcf8b8a
井上翼 <tsubasa.inoue.3b@stu.hosei.ac.jp>
2021-03-28 15:23:09 +0900

    create button using props

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,10 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import Header from './Header'
 
 
 export const siteTitle = 'Round Robin Home Page'
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home, }) {
   return (
     <div>
       <Head>
@@ -24,41 +23,7 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Header></Header>
-      {/* <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header> */}
+
       <main>{children}</main>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 import Image from 'next/image'
-import Link from 'next/link'
 import Styles from '../styles/index.module.scss'
+import Button from '../components/Button.jsx'
 
 
 export async function getStaticProps(){
@@ -35,12 +35,8 @@ export default function Home() {
           <p className={Styles.fontAdjust}>Live Freely</p>
           <p className={Styles.fontAdjust}>Grow Up With Friends!!</p>
           <p id={Styles.subPassage} className="fs-6 fw-bolder">-駆け出しエンジニアの共同コミュニティ-</p>
-          <Link href="#">
-            <a className={Styles.greenButton}>メンバーになる</a>
-          </Link>
-          <Link href="#">
-            <a className={Styles.blueButton}>詳しくみる</a>
-          </Link> 
+          <div id={Styles.toMember}><Button　title="メンバーになる" path="/Member"></Button></div>
+          <div id={Styles.details}><Button　title="詳細をみる"　path="/About"></Button></div>
         </div>
       </div>
     </Layout>
